0ab8913df0cc3615f7978e8557823e83d36cfa4d
JohnFreakKennedy <charliesweetuk@gmail.com>
2023-07-17 13:09:09 +0300

    Initial commit

---
+++ FA-Automation/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FA_Automation
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
